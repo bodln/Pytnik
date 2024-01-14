b4d6de2d1434efab970d538827304d7833904fdd
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2024-01-14 16:04:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,412 @@
+// Import statements for React, hooks, and styles
+import React, { useEffect, useRef, useState } from 'react';
+import Client, { Agent, IPoint, IStep, MapData } from "./Client";
+import Terrain from "./assets/maps/terrain_upscaled.png";
+import Coin from "./assets/icons/coin.png";
+import Aki from "./assets/icons/Aki.png";
+import Uki from "./assets/icons/Uki.png";
+import Jocke from "./assets/icons/Jocke.png";
+import Micko from "./assets/icons/Micko.png";
+import "./App.css";
+
+// Interface for AgentInfo
+interface AgentInfo {
+  name: Agent;
+  icon: string;
+}
+
+// Main App component
+const App: React.FC = () => {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const [agents, setAgents] = useState<AgentInfo[]>([
+    { name: "Aki", icon: Aki },
+    { name: "Uki", icon: Uki },
+    { name: "Jocke", icon: Jocke },
+    { name: "Micko", icon: Micko },
+  ]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [mapsData, setMapsData] = useState<MapData[] | null>(null);
+  const [selectedMap, setSelectedMap] = useState<MapData | null>(null);
+  const [selectedAgent, setSelectedAgent] = useState<AgentInfo>(null);
+  const [agentPosition, setAgentPosition] = useState<IPoint>({ x: 0, y: 0 });
+  const [agentMoving, setAgentMoving] = useState<boolean>(false);
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [currentNode, setCurrentNode] = useState<number>(0);
+  const [steps, setSteps] = useState<IStep[] | null>(null);
+  const [hoveredCoin, setHoveredCoin] = useState<number | null>(null);
+  const [graph, setGraph] = useState<number[][] | null>(null);
+  const [sumCost, setSumCost] = useState<number>(0);
+  const [stepMode, setStepMode] = useState<boolean>(true);
+  const [pauseSimulation, setPauseSimulation] = useState<boolean>(false);
+  const [pauseIndex, setPauseIndex] = useState<number>(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer | null>(null);
+  const [inTransition, setInTransition] = useState<boolean>(false);
+
+  const scaleCoinX = (x: number): number => {
+    if (!mapRef.current) return 0;
+    return (x / 1000) * mapRef.current.clientWidth;
+  };
+
+  const scaleCoinY = (y: number): number => {
+    if (!mapRef.current) return 0;
+    return (y / 600) * mapRef.current.clientHeight;
+  };
+
+  const setAgent = (agent: AgentInfo): void => {
+    setSelectedAgent(agent);
+    calculateSteps();
+  };
+
+  const moveAgentForward = (): void => {
+    if (!selectedMap) return;
+    if (currentStep === selectedMap.coins.length) {
+      moveAgent(0);
+      setSumCost(0);
+    } else {
+      moveAgent(currentStep + 1);
+      setSumCost(sumCost + steps[currentStep + 1].cost);
+    }
+  };
+
+  const moveAgentBackward = (): void => {
+    if (currentStep === 0) {
+      return;
+    } else {
+      moveAgent(currentStep - 1);
+      setSumCost(sumCost - steps[currentStep].cost)
+    }
+  };
+
+  const moveAgent = (step: number): void => {
+    if (!steps) return;
+    setCurrentNode(steps[step].to_node);
+    updateAgentPosition(selectedMap.coins[currentNode]);
+    setCurrentStep(step);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        if (!stepMode || agentMoving) return
+        event.preventDefault();
+        moveAgentForward();
+      } else if (event.key === "ArrowLeft") {
+        if (!stepMode || agentMoving) return
+        event.preventDefault();
+        moveAgentBackward();
+      } else if (event.key === "Enter") {
+        if (!selectedMap || agentMoving || !steps || steps.length < 0) return;
+        event.preventDefault();
+        setSumCost(0)
+        for (let i = 0; i < steps.length; i++) {
+          moveAgent(i);
+          setSumCost(prevSumCost => prevSumCost + steps[i].cost);
+        }
+      } else if ((event.key === "S" || event.key === "s")) {
+        setStepMode(!stepMode)
+      } else if (event.key === " ") {
+        event.preventDefault();
+        if (!selectedMap || !steps) return;
+    
+        if(!inTransition){
+          setPauseSimulation(agentMoving)
+        }
+        
+        if(!agentMoving){
+          //setSumCost(0)
+          //moveAgent(0);
+          setAgentMoving(true)
+          let i = pauseIndex
+
+          if(i == 0) setSumCost(0)
+
+          const interval = setInterval(() => {
+            if (i >= steps.length) {
+              setPauseSimulation(false);
+              setAgentMoving(false)
+              clearInterval(interval);
+              setPauseIndex(0)
+              return
+            }
+            console.log("Interval: " + i)
+            setInTransition(true)
+            setPauseIndex(i)
+            moveAgent(i)
+            setSumCost(prevSumCost => prevSumCost + steps[i].cost);
+            i++
+            setInTransition(false)
+          }, 600);
+
+          setIntervalId(interval)
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [moveAgentForward, moveAgentBackward, selectedMap]);
+
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+  useEffect(() => {
+    // console.log("Graph:" )
+    // console.log(graph)
+  }, [graph]);
+
+  useEffect(() => {
+    setupMaps();
+  }, []);
+
+  useEffect(() => {
+    console.log("Current node: " + currentNode)
+  }, [currentNode]);
+
+  useEffect(() => {
+    // console.log("agent moving:")
+    // console.log(agentMoving)
+  }, [agentMoving]);
+
+  useEffect(() => {
+    if(pauseSimulation && intervalId != null && !inTransition){
+      clearInterval(intervalId)
+      setIntervalId(null)
+      setAgentMoving(false)
+    }
+  }, [pauseSimulation]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (selectedMap != null && selectedAgent != null) {
+        await calculateSteps();
+        
+        if (steps != null && steps.length > 0) {
+          setCurrentStep(0);
+          setCurrentNode(steps[0].to_node);
+          updateAgentPosition(selectedMap.coins[currentNode]);
+        }
+      }
+
+      if(selectedMap != null){
+        setGraph(await Client.getGraph(selectedMap.map_name));
+      }
+    };
+
+    setSumCost(0)
+    fetchData();
+  }, [selectedMap]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (selectedAgent != null) {
+        await calculateSteps();
+
+        if (steps != null && steps.length > 0) {
+          setCurrentStep(0);
+          setCurrentNode(steps[0].to_node);
+          updateAgentPosition(selectedMap.coins[currentNode]);
+        }
+      }
+    };
+
+    setSumCost(0)
+    fetchData();
+  }, [selectedAgent]);
+
+  useEffect(() => {
+    if (mapsData != null) {
+      setSelectedMap(mapsData[0])
+    }
+  }, [mapsData])
+
+  useEffect(() => {
+    if (steps != null) {
+      // console.log("Steps calculated")
+      // console.log(steps)
+    }
+  }, [steps])
+
+  const setupMaps = async (): Promise<void> => {
+    setLoading(true);
+
+    try {
+      setMapsData(await Client.getMapsData());
+      await calculateSteps();
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
+  const selectMap = (map: MapData): void => {
+    setSelectedMap(map);
+    setAgentPosition(map.coins[0]);
+  };
+
+  const calculateSteps = async (): Promise<void> => {
+    if (!selectedMap) return;
+
+    setCurrentStep(0);
+    setCurrentNode(0);
+
+    updateAgentPosition(selectedMap.coins[currentNode]);
+    if (selectedAgent != null) setSteps(await Client.calculateSteps(selectedMap.map_name, selectedAgent.name));
+  };
+
+  const updateAgentPosition = async (point: IPoint): Promise<void> => {
+    if (mapRef.current) {
+      const x = scaleCoinX(point.x);
+      const y = scaleCoinY(point.y);
+      setAgentPosition({ x, y });
+    }
+  };
+
+  /* Add a function to get the tooltip content based on the agent's name */
+  const getAgentTooltip = (agentName: string): string => {
+    switch (agentName) {
+      case "Aki":
+        return "Agent koristi strategiju pohlepne pretrage po dubini...";
+      case "Jocke":
+        return "Agent koristi brute-force strategiju tako što generiše sve moguće putanje...";
+      case "Uki":
+        return "Agent koristi strategiju grananja i ograničavanja...";
+      case "Micko":
+        return "Agent koristi A* strategiju pretraživanja, pri čemu se za heurističku funkciju koristi minimalno obuhvatno stablo...";
+      default:
+        return "";
+    }
+  };
+
+  const navigateToPreviousMap = () => {
+    if (mapsData && selectedMap && !agentMoving) {
+      const currentIndex = mapsData.findIndex((map) => map.map_name === selectedMap.map_name);
+      const previousIndex = currentIndex === 0 ? mapsData.length - 1 : currentIndex - 1;
+  
+      // Only change the map if it's not the first map
+      if (currentIndex !== 0) {
+        setSelectedMap(mapsData[previousIndex]);
+      }
+    }
+  };
+  
+  const navigateToNextMap = () => {
+    if (mapsData && selectedMap && !agentMoving) {
+      const currentIndex = mapsData.findIndex((map) => map.map_name === selectedMap.map_name);
+      const nextIndex = currentIndex === mapsData.length - 1 ? 0 : currentIndex + 1;
+  
+      // Only change the map if it's not the last map
+      if (currentIndex !== mapsData.length - 1) {
+        setSelectedMap(mapsData[nextIndex]);
+      }
+    }
+  };
+
+  return (
+    <div id="home">
+      <div className="title">Pytnik</div>
+      <div className='container'>
+        <div className={`map${selectedMap ? ' margin-bottom' : ''}`} ref={mapRef}>
+          <img src={Terrain} alt="Terrain" />
+
+          <div className="map-overlay">
+            <div className="footer"></div>
+          </div>
+
+          {selectedMap?.coins.map((coin, index) => (
+            <div
+              className="coin"
+              key={`coin-${index}`}
+              style={{
+                left: `${scaleCoinX(coin.x)}px`,
+                top: `${scaleCoinY(coin.y)}px`,
+              }}
+              onMouseEnter={() => {
+                setHoveredCoin(index);
+              }}
+              onMouseLeave={() => {
+                setHoveredCoin(null);
+              }}
+            >
+              <img src={Coin} alt="Coin" />
+              <span>{index}</span>
+              {hoveredCoin == index && (
+                <div className="hover-message">
+                  {selectedMap.coins.map((otherCoin, otherIndex) => (
+                    otherIndex !== index && (
+                      <p key={`cost-${otherIndex}`}>
+                      {`Cena do ${otherIndex}: ${graph[index][otherIndex]}`}
+                    </p>
+                    )
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+
+          {selectedAgent != null && (
+            <div
+              className="agent"
+              style={{
+                left: `${scaleCoinX(selectedMap?.coins[currentNode].x)}px`,
+                top: `${scaleCoinY(selectedMap?.coins[currentNode].y)}px`,
+              }}
+            >
+              <img src={selectedAgent.icon} alt={selectedAgent.name} />
+            </div>
+          )}
+        </div>
+        <div className="header">
+          {agents.map((agent) => (
+            <div
+              className="agent-selector"
+              key={agent.name}
+              onClick={() => setAgent(agent)}
+            >
+              <img src={agent.icon} alt={agent.name} />
+              <p>{agent.name}</p>
+              <div className="agent-tooltip">
+                {getAgentTooltip(agent.name)}
+              </div>
+            </div>
+          ))}
+        </div>
+        {selectedMap && (
+          <div className='steps-container'>
+            <div className="steps-title">Koraci:</div>
+            <div className="steps">
+              {Array.from({ length: currentStep + 1 }).map((_, num) => (
+                steps?.[num].from_node !== steps?.[num].to_node && (
+                  <div className="step" key={`step-${num}`}>
+                    <p>
+                      {`Korak: ${steps?.[num].step} | ${steps?.[num].from_node} -> ${steps?.[num].to_node} | Cena: ${steps?.[num].cost}`}
+                    </p>
+                  </div>
+                )
+              ))}
+            </div>
+            <div className="steps-footer">Ukupna cena: {sumCost}</div>
+          </div>
+        )}
+      </div>
+
+      <div className='controls'>
+        <p>Pokreni simulaciju - Razmak</p>
+        <p>Prikaži konačan rezultat - Enter</p>
+        <p>Uključi/Isključi korake na strelice - S ({stepMode ? ("Uključeno") : ("Isključeno")})</p>
+      </div>
+
+      {selectedMap ? (
+        <div className="maps-control-container">
+          <p className='clickable' onClick={navigateToPreviousMap}>{selectedMap.map_name === mapsData[0].map_name ? '/' : '<<'}</p>
+          <p className='map-name'>{selectedMap.map_name}</p>
+          <p className='clickable' onClick={navigateToNextMap}>{selectedMap.map_name === mapsData[mapsData.length - 1].map_name ? '/' : '>>'}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Client.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+export type Agent = "Aki" | "Micko" | "Uki" | "Jocke";
+
+const apiUrl = "http://127.0.0.1:8000" || "http://localhost:8000";
+axios.defaults.baseURL = apiUrl;
+
+export interface MapData {
+  map_name: string;
+  coins: Array<IPoint>;
+}
+
+export interface IPoint {
+  x: number;
+  y: number;
+}
+
+export interface IStep {
+  step: number;
+  to_node: number;
+  from_node: number;
+  cost: number;
+}
+
+export default class Client {
+  static async getMapsData(): Promise<Array<MapData>> {
+    try {
+      const response = await axios.get("/maps/");
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching map data:", error);
+      throw error;
+    }
+  }
+
+  static async calculateSteps(mapName: string, agent: Agent): Promise<IStep[]> {
+    try {
+      const response = await axios.post("/calculate/", {
+        map: mapName,
+        algorithm: agent,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error calculating steps:", error);
+      throw error;
+    }
+  }
+
+  static async getGraph(mapName: string): Promise<any> {
+    try {
+      const response = await axios.post("/graph/", {
+        map: mapName,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching graph:", error);
+      throw error;
+    }
+  }
+}
